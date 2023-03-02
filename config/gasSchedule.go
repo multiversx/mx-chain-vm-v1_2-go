94d6f0ba4d00c07ea7c58d605bd0900f2b9df7b6
BeniaminDrasovean <beniamin.drasovean@elrond.network>
2023-03-02 12:22:14 +0200

    renaming

--- config/gasSchedule.go
+++ config/gasSchedule.go
@@ -145,8 +145,8 @@ func FillGasMap_BuiltInCosts(value uint64) map[string]uint64 {
 	gasMap["ESDTNFTAddUri"] = value
 	gasMap["ESDTNFTUpdateAttributes"] = value
 	gasMap["ESDTNFTMultiTransfer"] = value
-	gasMap["TrieLoad"] = value
-	gasMap["TrieStore"] = value
+	gasMap["TrieLoadPerNode"] = value
+	gasMap["TrieStorePerNode"] = value
 
 	return gasMap
 }
